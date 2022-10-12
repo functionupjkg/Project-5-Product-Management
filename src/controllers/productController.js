e8bb6b72fb42a1bdeb42a1ff8cfe06ded0167ffc
Gautam <gautamdhakate1234@gmail.com>
2022-10-12 15:43:50 +0530

    create ProductModel

---
+++ src/controllers/productController.js
@@ -0,0 +1,1 @@
+const productModel = require("../Models/productModel")
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const userController = require("../controllers/userController")
+const productController = require("../controllers/productController")
 const {authentic , authorize} = require("../middleWare/auth")
 
 
@@ -10,6 +11,8 @@ router.post("/test-me", (req,res)=>{
     res.send({msg : "ok All Fine"})
 })
 
+//----------------------------------userController-----------------------------------
+
 router.post("/register",userController.createUser)
 
 router.post("/login" , userController.loginUser)
@@ -17,6 +20,8 @@ router.post("/login" , userController.loginUser)
 router.get("/user/:userId/profile" , authentic , authorize,  userController.getUserById)
 
 router.put("/user/:userId/profile", authentic, authorize, userController.updateUser)
+
+//--------------------------- productController----------------------------------------
 
 
 
